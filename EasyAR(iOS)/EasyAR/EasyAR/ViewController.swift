5adec838635c74ea66b67f62d50e4d0f2ff632e4
AzeemMuzammil <azeemmuzammil@yahoo.com>
2019-10-19 21:56:22 +0530

    initial commit for iOS application files

---
+++ EasyAR(iOS)/EasyAR/EasyAR/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  EasyAR
+//
+//  Created by Azeem Muzammil on 10/19/19.
+//  Copyright © 2019 Azeem Muzammil. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
