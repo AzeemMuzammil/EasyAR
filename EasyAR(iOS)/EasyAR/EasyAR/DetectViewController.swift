13d8589e710159fc919dc1435d6c3a28dff85326
Azeem Muzammil <azeemmuzammil@yahoo.com>
2019-12-21 15:40:01 +0530

    QR detection added

--- EasyAR(iOS)/EasyAR/EasyAR/DetectViewController.swift
+++ EasyAR(iOS)/EasyAR/EasyAR/DetectViewController.swift
@@ -9,17 +9,145 @@
 import UIKit
 import ARKit
 import Vision
+import SceneKit
 
-class DetectViewController: UIViewController {
+class DetectViewController: UIViewController, ARSCNViewDelegate,ARSessionDelegate {
     
     let configuration = ARWorldTrackingConfiguration()
     
     @IBOutlet weak var sceneView: ARSCNView!
+    
+    var qrRequests = [VNRequest]()
+    var detectedDataAnchor: ARAnchor?
+    var processing = false
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.sceneView.debugOptions = [.showFeaturePoints, .showWorldOrigin]
         self.sceneView.session.run(configuration)
+        
+        sceneView.delegate = self
+        sceneView.session.delegate=self
+        
+        startQrCodeDetection()
+    }
+    
+    func startQrCodeDetection() {
+        // Create a Barcode Detection Request
+        let request = VNDetectBarcodesRequest(completionHandler: self.requestHandler)
+        // Set it to recognize QR code only
+        request.symbologies = [.QR]
+        self.qrRequests = [request]
+    }
+    
+    func requestHandler(request: VNRequest, error: Error?) {
+        // Get the first result out of the results, if there are any
+        if let results = request.results, let result = results.first as? VNBarcodeObservation {
+            print(result.payloadStringValue ?? "No String")
+            guard result.payloadStringValue != nil else {return}
+            // Get the bounding box for the bar code and find the center
+            var rect = result.boundingBox
+            // Flip coordinates
+            rect = rect.applying(CGAffineTransform(scaleX: 1, y: -1))
+            rect = rect.applying(CGAffineTransform(translationX: 0, y: 1))
+            // Get center
+            let center = CGPoint(x: rect.midX, y: rect.midY)
+            
+            DispatchQueue.main.async {
+                self.hitTestQrCode(center: center)
+                self.processing = false
+            }
+        } else {
+            self.processing = false
+        }
+    }
+    
+    func hitTestQrCode(center: CGPoint) {
+        if let hitTestResults = sceneView?.hitTest(center, types: [.featurePoint] ),
+            let hitTestResult = hitTestResults.first {
+            if let detectedDataAnchor = self.detectedDataAnchor,
+                let node = self.sceneView.node(for: detectedDataAnchor) {
+                _ = node.position
+                node.transform = SCNMatrix4(hitTestResult.worldTransform)
+                
+            } else {
+                // Create an anchor. The node will be created in delegate methods
+                self.detectedDataAnchor = ARAnchor(transform: hitTestResult.worldTransform)
+                self.sceneView.session.add(anchor: self.detectedDataAnchor!)
+            }
+        }
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+
+        // Pause the view's session
+        sceneView.session.pause()
+    }
+    
+    public func session(_ session: ARSession, didUpdate frame: ARFrame) {
+        DispatchQueue.global(qos: .userInitiated).async {
+            do {
+                if self.processing {
+                    return
+                }
+                self.processing = true
+                // Create a request handler using the captured image from the ARFrame
+                let imageRequestHandler = VNImageRequestHandler(cvPixelBuffer: frame.capturedImage, options: [:])
+                // Process the request
+                try imageRequestHandler.perform(self.qrRequests)
+            } catch {
+                
+            }
+        }
+    }
+
+    // MARK: - ARSCNViewDelegate
+    
+
+    // Override to create and configure nodes for anchors added to the view's session.
+
+    
+    func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
+
+        // If this is our anchor, create a node
+        if self.detectedDataAnchor?.identifier == anchor.identifier {
+            let sphere = SCNSphere(radius: 0.2)
+            sphere.firstMaterial?.diffuse.contents = UIColor.red
+            let sphereNode = SCNNode(geometry: sphere)
+            sphereNode.transform = SCNMatrix4(anchor.transform)
+            return sphereNode
+        }
+        return nil
+    }
+    
+    func addView() ->SCNNode{
+
+        
+        
+        let skScene = SKScene(size: CGSize(width: 200, height: 200))
+        skScene.backgroundColor = UIColor.clear
+        
+        let rectangle = SKShapeNode(rect: CGRect(x: 0, y: 0, width: 200, height: 200), cornerRadius: 10)
+        rectangle.fillColor = #colorLiteral(red: 0.807843148708344, green: 0.0274509806185961, blue: 0.333333343267441, alpha: 1.0)
+        rectangle.strokeColor = #colorLiteral(red: 0.439215689897537, green: 0.0117647061124444, blue: 0.192156866192818, alpha: 1.0)
+        rectangle.lineWidth = 5
+        rectangle.alpha = 0.4
+        let labelNode = SKLabelNode(text: "Hello World")
+        labelNode.fontSize = 20
+        labelNode.fontName = "Arial"
+        labelNode.position = CGPoint(x:0,y:0)
+        skScene.addChild(rectangle)
+        skScene.addChild(labelNode)
+        
+        
+        let plane = SCNPlane(width: 0.20, height: 0.20)
+        let material = SCNMaterial()
+        material.isDoubleSided = true
+        material.diffuse.contents = skScene
+        plane.materials = [material]
+        let planeNode = SCNNode(geometry: plane)
+
+        return planeNode
     }
 
 }
